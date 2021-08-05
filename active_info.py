e6e3856c6cefef01dc3e355d77e62022394014b4
f-inx <80400163+f-inx@users.noreply.github.com>
2021-08-05 11:01:59 -0400

    Create active_info.py
    copy paste from the notes

---
+++ active_info.py
@@ -0,0 +1,24 @@
+!/bin/python3
+
+#The modules needed
+import nmap
+import sys
+import time
+
+#basic scan in port 80
+nm_scan = nmap.PortScanner()
+print("\nRunning.......\n")
+nm_scanner = nm_scan.scan(sys.argv[1],'80', arguments='-O')
+
+#clean the output up
+host_is_up = "The host is: "+nm_scanner['scan'][sys.argv[1]]['status']['state']+"\n."
+port_open = "The port 80 is: "+nm_scanner['scan'][sys.argv[1]]['tcp'][80]['state']+"\n."
+method_scan = "The method of scanning is: "+nm_scanner['scan'][sys.argv[1]]['tcp'][80]['reason']+"\n."
+guessed_os = "There is a %s percent chance that the host is running %s"%(nm_scanner['scan'][sys.argv[1]]['osmatch'][0]['accuracy'], nm_scanner['scan'][sys.argv[1]]['osmatch'][0]['name'])+"\n."
+
+#send the output into a file with <ip>.txt
+with open("%s.txt"%sys.argv[1], 'w') as f:
+	f.write(host_is_up+port_open+method_scan+guessed_os)
+	f.write("\nReport generated "+time.strftime("%Y-%m-%d_%H:%M:%S GMT", time.gmtime()))
+	
+print("\nFinished...")
